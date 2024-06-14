8be03f779213ebf8603c35bb2e182c894b0ea9e5
Joao Marcos Fragallo De Souza <111794510+jmfragallo@users.noreply.github.com>
2024-06-13 22:22:09 -0300

    Primeira activit

---
+++ app/src/main/java/com/jmfragallo/jmviagens/ui/activity/ListaPacotesActivity.java
@@ -0,0 +1,16 @@
+package com.jmfragallo.jmviagens.ui.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.jmfragallo.jmviagens.R;
+
+public class ListaPacotesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_pacotes);
+    }
+}
