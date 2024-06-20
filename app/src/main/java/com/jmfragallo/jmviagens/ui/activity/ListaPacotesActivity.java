39caf737aa51079a216638aabb6732a07b5678bf
Joao Marcos Fragallo De Souza <111794510+jmfragallo@users.noreply.github.com>
2024-06-19 22:16:59 -0300

    ...

--- app/src/main/java/com/jmfragallo/jmviagens/ui/activity/ListaPacotesActivity.java
+++ app/src/main/java/com/jmfragallo/jmviagens/ui/activity/ListaPacotesActivity.java
@@ -3,14 +3,31 @@ package com.jmfragallo.jmviagens.ui.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
 
 import com.jmfragallo.jmviagens.R;
+import com.jmfragallo.jmviagens.dao.PacoteDao;
+import com.jmfragallo.jmviagens.model.Pacote;
+import com.jmfragallo.jmviagens.ui.activity.adapter.ListaPacotesAdapter;
+
+import java.util.List;
 
 public class ListaPacotesActivity extends AppCompatActivity {
+
+    public static final String TITULO_APPBAR = "Pacotes";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_pacotes);
+
+        setTitle(TITULO_APPBAR);
+        configuraLista();
+    }
+
+    private void configuraLista() {
+        ListView listaDePacotes = findViewById(R.id.lista_pacotes_listview);
+        List<Pacote> pacotes = new PacoteDao().lista();
+        listaDePacotes.setAdapter(new ListaPacotesAdapter(pacotes, this));
     }
 }
