10d114e42596330b738a4d7b0d0a01176d381485
Joao Marcos Fragallo De Souza <111794510+jmfragallo@users.noreply.github.com>
2024-06-20 21:58:36 -0300

    ...

--- app/src/main/java/com/jmfragallo/jmviagens/ui/activity/ResumoPacoteActivity.java
+++ app/src/main/java/com/jmfragallo/jmviagens/ui/activity/ResumoPacoteActivity.java
@@ -2,9 +2,18 @@ package com.jmfragallo.jmviagens.ui.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.jmfragallo.jmviagens.R;
+import com.jmfragallo.jmviagens.model.Pacote;
+import com.jmfragallo.jmviagens.util.DiasUtil;
+import com.jmfragallo.jmviagens.util.MoedaUtil;
+import com.jmfragallo.jmviagens.util.ResourceUtil;
+
+import java.math.BigDecimal;
 
 public class ResumoPacoteActivity extends AppCompatActivity {
 
@@ -12,5 +21,25 @@ public class ResumoPacoteActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resumo_pacote);
+
+        Pacote pacoteSaoPaulo = new Pacote("São Paulo", "sao_paulo_sp",
+                2, new BigDecimal("243.99"));
+
+        TextView local = findViewById(R.id.resumo_pacote_local);
+        local.setText(pacoteSaoPaulo.getLocal());
+
+        ImageView imagem = findViewById(R.id.resumo_pacote_imagem);
+        Drawable drawableDoPacote = ResourceUtil
+                .devolveDrawable(this, pacoteSaoPaulo.getImagem());
+        imagem.setImageDrawable(drawableDoPacote);
+
+        TextView dias = findViewById(R.id.resumo_pacote_dias);
+        String diasEmTexto = DiasUtil.formataEmTexto(pacoteSaoPaulo.getDias());
+        dias.setText(diasEmTexto);
+
+        TextView preco = findViewById(R.id.resumo_pacote_preco);
+        String moedaBrasileira = MoedaUtil
+                .formataParaBrasileiro(pacoteSaoPaulo.getPreco());
+        preco.setText(moedaBrasileira);
     }
 }
